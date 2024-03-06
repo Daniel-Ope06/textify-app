b4bdc477e30e256f998223ef6ddd9ef3dcaaed95
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-06 11:43:53 +0100

    Create `creator-info` component

---
+++ src/app/domains/home/feature/creator-info/creator-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creator-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './creator-info.component.html',
+  styleUrl: './creator-info.component.scss'
+})
+export class CreatorInfoComponent {
+
+}
