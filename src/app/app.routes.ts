d2c52080bcabb688706807a9f79bdfb034762d88
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-06 11:40:10 +0100

    Add `home` route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        title: 'Textify',
+        loadComponent: () => import('./domains/home/home.component').then(c => c.HomeComponent)
+    },
+];
