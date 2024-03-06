c8338692bd20787f9675e31b663d05a7384b6b7f
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-06 15:18:35 +0100

    Import `promo-banner` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CreatorInfoComponent } from './feature/creator-info/creator-info.component';
+import { PromoBannerComponent } from './feature/promo-banner/promo-banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CreatorInfoComponent],
+  imports: [CreatorInfoComponent, PromoBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
