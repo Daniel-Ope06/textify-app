ae33bc0beec42f080770fc2ac9e9ca94af258d85
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-06 12:24:37 +0100

    Import `creator-info` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CreatorInfoComponent } from './feature/creator-info/creator-info.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CreatorInfoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
