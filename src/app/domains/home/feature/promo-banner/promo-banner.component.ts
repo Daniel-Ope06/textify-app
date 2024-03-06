fc0fa6a576e55f5f201e6cf478117865707ea281
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-06 15:13:27 +0100

    Create `promo-banner` component

---
+++ src/app/domains/home/feature/promo-banner/promo-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'promo-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './promo-banner.component.html',
+  styleUrl: './promo-banner.component.scss'
+})
+export class PromoBannerComponent {
+
+}
