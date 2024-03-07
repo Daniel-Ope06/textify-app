6a26da030f3ce1fa4715554e86aff30d40781894
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-07 10:53:57 +0100

    Import `how-it-works` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CreatorInfoComponent } from './feature/creator-info/creator-info.component';
+import { HowItWorksComponent } from './feature/how-it-works/how-it-works.component';
 import { PromoBannerComponent } from './feature/promo-banner/promo-banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CreatorInfoComponent, PromoBannerComponent],
+  imports: [CreatorInfoComponent, HowItWorksComponent, PromoBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
