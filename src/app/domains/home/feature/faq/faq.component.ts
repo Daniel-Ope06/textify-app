7ce8c1dab83082a0a717587650220d64f0a8bc20
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-07 17:41:59 +0100

    Set up `faq` component
    - import `loading-spinner` into faq`

--- src/app/domains/home/feature/faq/faq.component.ts
+++ src/app/domains/home/feature/faq/faq.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FaqService } from '../../data-access/faq.service';
+import { LoadingSpinnerComponent } from '../../../shared/ui/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'faq',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
-export class FaqComponent {
+export class FaqComponent implements OnInit {
+  private faqService: FaqService = inject(FaqService);
+  faqs: { question: string, answer: string, isOpen: boolean }[] = [];
 
+  ngOnInit() {
+    this.faqService.getAll().then((faqs) => {
+      for (let faq of faqs) {
+        this.faqs.push({ question: faq.question, answer: faq.answer, isOpen: false });
+      }
+    });
+  }
+
+  toggleFaq(index: number) {
+    this.faqs[index].isOpen = !this.faqs[index].isOpen;
+  }
 }
