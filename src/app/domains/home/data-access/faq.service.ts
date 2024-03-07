4637708d7b0c64e92a053ab7286442d3d867d86a
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-07 16:32:49 +0100

    Set up `faq` service

--- src/app/domains/home/data-access/faq.service.ts
+++ src/app/domains/home/data-access/faq.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { Faq } from './faq.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FaqService {
+  firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  async getAll(): Promise<Faq[]> {
+    let faqs: Faq[] = [];
+    const querySnapshot = await getDocs(collection(this.firestore, 'faq'));
+    querySnapshot.forEach((doc) => {
+      faqs.push(doc.data() as Faq);
+    });
+    return faqs;
+  }
 }
