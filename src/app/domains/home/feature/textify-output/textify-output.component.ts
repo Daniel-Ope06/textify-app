c688ac54658163b0a6a7b099e4bc93604a401ba8
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-08 16:45:30 +0100

    Create `textify-output` component

---
+++ src/app/domains/home/feature/textify-output/textify-output.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'textify-output',
+  standalone: true,
+  imports: [],
+  templateUrl: './textify-output.component.html',
+  styleUrl: './textify-output.component.scss'
+})
+export class TextifyOutputComponent {
+
+}
