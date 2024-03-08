b6a3d8f629a7d973baa91fd47561dfe5e913cc8e
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-08 20:37:23 +0100

    Add `WordArtService` to `textify-input`

--- src/app/domains/home/feature/textify-input/textify-input.component.ts
+++ src/app/domains/home/feature/textify-input/textify-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { WordArtService } from '../../../shared/service/word-art/word-art.service';
 
 @Component({
   selector: 'textify-input',
@@ -9,8 +10,14 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './textify-input.component.scss'
 })
 export class TextifyInputComponent {
+  @Output() createArtEvent = new EventEmitter<string>();
+  artService: WordArtService = inject(WordArtService);
+
   input: { text: string, symbol: string } = { text: '', symbol: '' };
   placeholder: { text: string, symbol: string } = { text: 'Textify', symbol: '*::*' };
 
-  onSubmit() {}
+  onSubmit() {
+    let wordArt: string = this.artService.convertToWordArt(this.input.text, this.input.symbol);
+    this.createArtEvent.emit(wordArt);
+  }
 }
