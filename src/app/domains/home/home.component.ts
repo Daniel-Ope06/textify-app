85a35411b0c38ffc4de209c72e0de8c8f4b0e68e
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-08 17:39:17 +0100

    Import `textify-output` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -4,11 +4,12 @@ import { FaqComponent } from './feature/faq/faq.component';
 import { HowItWorksComponent } from './feature/how-it-works/how-it-works.component';
 import { PromoBannerComponent } from './feature/promo-banner/promo-banner.component';
 import { TextifyInputComponent } from './feature/textify-input/textify-input.component';
+import { TextifyOutputComponent } from './feature/textify-output/textify-output.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CreatorInfoComponent, FaqComponent, HowItWorksComponent, PromoBannerComponent, TextifyInputComponent],
+  imports: [CreatorInfoComponent, FaqComponent, HowItWorksComponent, PromoBannerComponent, TextifyInputComponent, TextifyOutputComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
