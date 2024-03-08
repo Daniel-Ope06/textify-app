e710621fdc793695174d12d4ede92ba497fc24da
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-08 14:49:16 +0100

    Create `textify-input` component

---
+++ src/app/domains/home/feature/textify-input/textify-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'textify-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './textify-input.component.html',
+  styleUrl: './textify-input.component.scss'
+})
+export class TextifyInputComponent {
+
+}
