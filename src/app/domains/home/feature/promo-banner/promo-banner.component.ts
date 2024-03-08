22b2c658d011f27ab7f00664f273423ad3669241
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-08 12:54:28 +0100

    Import `call-to-action` into `promo-banner`

--- src/app/domains/home/feature/promo-banner/promo-banner.component.ts
+++ src/app/domains/home/feature/promo-banner/promo-banner.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CallToActionComponent } from '../call-to-action/call-to-action.component';
 
 @Component({
   selector: 'promo-banner',
   standalone: true,
-  imports: [],
+  imports: [CallToActionComponent],
   templateUrl: './promo-banner.component.html',
   styleUrl: './promo-banner.component.scss'
 })
