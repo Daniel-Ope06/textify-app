d345fb1d6b8b669166e9c666079681b76006c7cc
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-08 13:14:14 +0100

    Import `call-to-action` into `how-it-works`

--- src/app/domains/home/feature/how-it-works/how-it-works.component.ts
+++ src/app/domains/home/feature/how-it-works/how-it-works.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CallToActionComponent } from '../call-to-action/call-to-action.component';
 
 @Component({
   selector: 'how-it-works',
   standalone: true,
-  imports: [],
+  imports: [CallToActionComponent],
   templateUrl: './how-it-works.component.html',
   styleUrl: './how-it-works.component.scss'
 })
