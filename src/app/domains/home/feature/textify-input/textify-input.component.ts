e889f9e2f5d8e9e0fc66e4fecbefd07152035182
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-08 16:41:46 +0100

    Set up `textify-input` component

--- src/app/domains/home/feature/textify-input/textify-input.component.ts
+++ src/app/domains/home/feature/textify-input/textify-input.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'textify-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './textify-input.component.html',
   styleUrl: './textify-input.component.scss'
 })
 export class TextifyInputComponent {
+  input: { text: string, symbol: string } = { text: '', symbol: '' };
+  placeholder: { text: string, symbol: string } = { text: 'Textify', symbol: '*::*' };
 
+  onSubmit() {}
 }
