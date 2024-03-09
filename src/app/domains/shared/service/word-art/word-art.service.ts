f7b369b5bcd80dab606a193bb068a7617046d957
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-09 12:48:54 +0100

    Create `getLine` in `wordArtService`

--- src/app/domains/shared/service/word-art/word-art.service.ts
+++ src/app/domains/shared/service/word-art/word-art.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Characters } from './characters/characters';
+
+// Alphabets
+import { A } from './characters/alphabet/english/a.alphabet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordArtService {
+  characterSet: string[] = ["A"];
+
   convertToWordArt(text: string, symbol: string): string {
     let wordArt: string = "";
 
     for (let i = 0; i < 10; i++){
-      wordArt += Characters.getLine(i, text, symbol) + "\n";
+      wordArt += this.getLine(i, text, symbol) + "\n";
     }
 
     return wordArt;
   }
+
+
+  private getLine(lineNumber: number, text: string, symbol: string): string {
+    let line: string = "";
+    text = text.toUpperCase();
+
+    for (let i = 0; i < text.length; i++) {
+        let character = text.charAt(i);
+        switch (character) {
+            case "A": line += A.getLine(lineNumber, symbol); break;
+        }
+    }
+
+    return line;
+  }
+
 }
