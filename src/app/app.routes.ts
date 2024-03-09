d8591e814b4fac1c363151d7cc0a0fff54ad1ea2
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-09 17:15:20 +0100

    Change route from `home` to ' '

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
         title: 'Textify',
         loadComponent: () => import('./domains/home/home.component').then(c => c.HomeComponent)
     },
