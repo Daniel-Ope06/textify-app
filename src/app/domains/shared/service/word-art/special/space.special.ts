88bc1e873f24d91fbadb3dd7c23de483b1b77e57
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-10 23:25:49 +0100

    Create special `space`

---
+++ src/app/domains/shared/service/word-art/special/space.special.ts
@@ -0,0 +1,21 @@
+export class Space {
+    static getLine(lineNumber: number, symbol: string): string{
+        let ____ = ' '.repeat(symbol.length);
+        let llll = symbol;
+        let line: string = "";
+
+        switch(lineNumber){
+            case 0: line = ____ + ____; break;
+            case 1: line = ____ + ____; break;
+            case 2: line = ____ + ____; break;
+            case 3: line = ____ + ____; break;
+            case 4: line = ____ + ____; break;
+            case 5: line = ____ + ____; break;
+            case 6: line = ____ + ____; break;
+            case 7: line = ____ + ____; break;
+            case 8: line = ____ + ____; break;
+            case 9: line = ____ + ____; break;
+        }
+        return line;
+    }
+}
--- src/app/domains/shared/service/word-art/word-art.service.ts
+++ src/app/domains/shared/service/word-art/word-art.service.ts
@@ -34,6 +34,7 @@ import { Equality } from './special/equality.special';
 import { Exclamation } from './special/exclamation.special';
 import { Hashtag } from './special/hashtag.special';
 import { Question } from './special/question.special';
+import { Space } from './special/space.special';
 import { Subtraction } from './special/subtraction.special';
 
 @Injectable({
@@ -46,7 +47,7 @@ export class WordArtService {
     // Numbers
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     // Specials
-    "+", ":", "=", "!", "#", "?", "-",
+    "+", ":", "=", "!", "#", "?", " ", "-",
   ];
 
   convertToWordArt(text: string, symbol: string): string {
@@ -101,6 +102,7 @@ export class WordArtService {
           case "!": line += Exclamation.getLine(lineNumber, symbol); break;
           case "#": line += Hashtag.getLine(lineNumber, symbol); break;
           case "?": line += Question.getLine(lineNumber, symbol); break;
+          case " ": line += Space.getLine(lineNumber, symbol); break;
           case "-": line += Subtraction.getLine(lineNumber, symbol); break;
         }
     }
