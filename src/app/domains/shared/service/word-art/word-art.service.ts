b1801fb465621d2b1327afb68c17a6677c4b32f9
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-10 22:48:04 +0100

    Create special `=`

--- src/app/domains/shared/service/word-art/word-art.service.ts
+++ src/app/domains/shared/service/word-art/word-art.service.ts
@@ -29,6 +29,7 @@ import { Nine } from './number/9.number';
 
 // Special
 import { Addition } from './special/addition.special';
+import { Equality } from './special/equality.special';
 import { Exclamation } from './special/exclamation.special';
 
 @Injectable({
@@ -41,7 +42,7 @@ export class WordArtService {
     // Numbers
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     // Specials
-    "+", "!",
+    "+", "=", "!",
   ];
 
   convertToWordArt(text: string, symbol: string): string {
@@ -91,6 +92,7 @@ export class WordArtService {
 
           // Specials
           case "+": line += Addition.getLine(lineNumber, symbol); break;
+          case "=": line += Equality.getLine(lineNumber, symbol); break;
           case "!": line += Exclamation.getLine(lineNumber, symbol); break;
         }
     }
