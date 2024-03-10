4004310075bc6bf0319fbabfb014e98e41fe0ef5
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-10 19:14:09 +0100

    Create alphabet `G`

--- src/app/domains/shared/service/word-art/word-art.service.ts
+++ src/app/domains/shared/service/word-art/word-art.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 
 // Alphabets
-import { A } from './alphabet/a.alphabet'; import { B } from './alphabet/b.alphabet'; import { C } from './alphabet/c.alphabet';
-import { D } from './alphabet/d.alphabet'; import { E } from './alphabet/e.alphabet'; import { F } from './alphabet/f.alphabet';
-
+import { A } from './alphabet/a.alphabet'; import { B } from './alphabet/b.alphabet';
+import { C } from './alphabet/c.alphabet'; import { D } from './alphabet/d.alphabet';
+import { E } from './alphabet/e.alphabet'; import { F } from './alphabet/f.alphabet';
+import { G } from './alphabet/g.alphabet';
 
 
 import { M } from './alphabet/m.alphabet';
@@ -32,13 +33,11 @@ export class WordArtService {
     for (let i = 0; i < text.length; i++) {
         let character = text.charAt(i);
         switch (character) {
-          case "A": line += A.getLine(lineNumber, symbol); break;
-          case "B": line += B.getLine(lineNumber, symbol); break;
-          case "C": line += C.getLine(lineNumber, symbol); break;
-          case "D": line += D.getLine(lineNumber, symbol); break;
-          case "E": line += E.getLine(lineNumber, symbol); break;
-          case "F": line += F.getLine(lineNumber, symbol); break;
-
+          // Alphabets
+          case "A": line += A.getLine(lineNumber, symbol); break; case "B": line += B.getLine(lineNumber, symbol); break;
+          case "C": line += C.getLine(lineNumber, symbol); break; case "D": line += D.getLine(lineNumber, symbol); break;
+          case "E": line += E.getLine(lineNumber, symbol); break; case "F": line += F.getLine(lineNumber, symbol); break;
+          case "G": line += G.getLine(lineNumber, symbol); break;
 
 
 
