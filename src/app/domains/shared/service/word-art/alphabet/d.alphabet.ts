bb02eba86e1cf33cb251d1a9d999d62349087ffb
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-10 18:54:39 +0100

    Create alphabet `D`

---
+++ src/app/domains/shared/service/word-art/alphabet/d.alphabet.ts
@@ -0,0 +1,21 @@
+export class D {
+    static getLine(lineNumber: number, symbol: string): string{
+        let ____ = ' '.repeat(symbol.length);
+        let llll = symbol; 
+        let line: string = "";
+
+        switch(lineNumber){
+            case 0: line = llll+llll+____+____; break;
+            case 1: line = llll+llll+____+____; break;
+            case 2: line = llll+____+llll+____; break;
+            case 3: line = llll+____+llll+____; break;
+            case 4: line = llll+____+llll+____; break;
+            case 5: line = llll+____+llll+____; break;
+            case 6: line = llll+____+llll+____; break;
+            case 7: line = llll+____+llll+____; break;
+            case 8: line = llll+llll+____+____; break;
+            case 9: line = llll+llll+____+____; break;
+        }
+        return line;
+    }
+}
--- src/app/domains/shared/service/word-art/word-art.service.ts
+++ src/app/domains/shared/service/word-art/word-art.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 
 // Alphabets
 import { A } from './alphabet/a.alphabet'; import { B } from './alphabet/b.alphabet'; import { C } from './alphabet/c.alphabet';
+import { D } from './alphabet/d.alphabet';
+
+
 
 import { M } from './alphabet/m.alphabet';
 
@@ -9,7 +12,7 @@ import { M } from './alphabet/m.alphabet';
   providedIn: 'root'
 })
 export class WordArtService {
-  characterSet: string[] = ["A", "B", "C",    "M"];
+  characterSet: string[] = ["A", "B", "C", "D",    "M"];
 
   convertToWordArt(text: string, symbol: string): string {
     let wordArt: string = "";
@@ -29,15 +32,15 @@ export class WordArtService {
     for (let i = 0; i < text.length; i++) {
         let character = text.charAt(i);
         switch (character) {
-            case "A": line += A.getLine(lineNumber, symbol); break;
-            case "B": line += B.getLine(lineNumber, symbol); break;
-            case "C": line += C.getLine(lineNumber, symbol); break;
+          case "A": line += A.getLine(lineNumber, symbol); break;
+          case "B": line += B.getLine(lineNumber, symbol); break;
+          case "C": line += C.getLine(lineNumber, symbol); break;
+          case "D": line += D.getLine(lineNumber, symbol); break;
 
 
 
 
-
-            case "M": line += M.getLine(lineNumber, symbol); break;
+          case "M": line += M.getLine(lineNumber, symbol); break;
         }
     }
 
