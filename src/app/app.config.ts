a622e41177d8d463bb7e3fd6048deacbbbcef8cd
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-14 09:28:57 +0100

    Move firebase app info to `environment.ts`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,10 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"textify-art","appId":"1:933785432280:web:173f6a66d99bac2d7fa7ad","storageBucket":"textify-art.appspot.com","apiKey":"AIzaSyCua9ksUvdWGTVWhXYAW4-sTRtYM2YYhQM","authDomain":"textify-art.firebaseapp.com","messagingSenderId":"933785432280","measurementId":"G-WVEFLGLST4"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
